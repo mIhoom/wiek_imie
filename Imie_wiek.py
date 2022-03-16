7455243d6a051618f2cfca6136deaf5d6253be3e
mIhoom <mdmichalski.poczta@gmail.com>
2022-03-16 22:35:19 +0100

    Another commit

---
+++ Imie_wiek.py
@@ -0,0 +1,5 @@
+name = input("Jak masz na imię? ")
+age  = input("Ile masz lat? ")
+print ("Twoje imię to " + name + " i masz " + age + " lat.")
+if int(age) >= 100:
+    print("Chyba nie jesteś zupełenie szczery, a jeśli jednak, to Gratualcje z okazji przeżytych lat!")
